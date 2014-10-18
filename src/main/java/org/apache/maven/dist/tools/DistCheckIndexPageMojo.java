cf75462a674519959b2066276c116ccad34207b9
Herve Boutemy <hboutemy@apache.org>
2014-10-18 15:55:55 +0000

    removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk@1632811 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/dist/tools/DistCheckIndexPageMojo.java
+++ src/main/java/org/apache/maven/dist/tools/DistCheckIndexPageMojo.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
